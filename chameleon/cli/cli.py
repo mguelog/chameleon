67ab1eb880353670afe66f7991e60ce8ac18a128
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-04-25 00:47:12 +0200

    feat(CLI): initial CLI

---
+++ chameleon/cli/cli.py
@@ -0,0 +1,8 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog='chameleon',
+    description='Chameleon Framework'
+)
+
+args = parser.parse_args()
---
+++ chameleon/main.py
@@ -0,0 +1,9 @@
+from cli.cli import args
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
