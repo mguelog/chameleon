463052e4309cce20317df303e32af1641e4c7fea
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-04-30 18:22:44 +0200

    feat(CLI): init command

--- chameleon/cli/cli.py
+++ chameleon/cli/cli.py
@@ -5,4 +5,17 @@ parser = argparse.ArgumentParser(
     description='Chameleon Framework'
 )
 
+subparsers = parser.add_subparsers(dest='command')
+
+init_parser = subparsers.add_parser(
+    'init',
+    description='Initialize simulation state',
+    help='initialize simulation state'
+)
+init_parser.add_argument(
+    '-o', '--overwrite',
+    help='overwrite current state',
+    action='store_true'
+)
+
 args = parser.parse_args()
--- chameleon/main.py
+++ chameleon/main.py
@@ -1,8 +1,15 @@
-from cli.cli import args
+from chameleon.cli.cli import args
+import subprocess, glob
 
 
 def main():
-    pass
+    if args.command == 'init':
+        if args.overwrite:
+            subprocess.run(['rm'] + glob.glob('*.sqlite'));
+        try:
+            subprocess.run(['python3', 'init.py'])
+        except FileNotFoundError:
+            print('Not init.py found')
 
 
 if __name__ == '__main__':
