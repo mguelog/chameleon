ba5d38600a6e8f8082740ac4cedd1150205653c3
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-04-30 02:13:59 +0200

    feat(setup): initial setup config

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chameleon',
+    version='0.0.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='mguelog',
+    author_email='',
+    description=''
+)
