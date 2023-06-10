579f053f176976613110182a7dc244007f0da18c
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-06-11 01:53:22 +0200

    feat(simulation): repeat simulation

--- chameleon/simulation/manager.py
+++ chameleon/simulation/manager.py
@@ -5,23 +5,28 @@ buffer = Buffer()
 
 class Manager:
 
-    def __init__(self, transitions, simulation):
+    def __init__(self, transitions, simulation, times, table):
         self.transitions = transitions
         self.simulation = simulation
+        self.times = times
+        self.table = table
+
         buffer.create()
 
     def simulate(self):
         print('Simulation started')
 
-        i = 0
-        action = buffer.read()
+        for i in range(self.times):
 
-        while i < len(self.simulation) and action != buffer.EXIT:
-
-            if action == buffer.FREE:
-                buffer.write(self.simulation[i])
-                i += 1
-
+            j = 0
             action = buffer.read()
+
+            while j < len(self.simulation) and action != buffer.EXIT:
+
+                if action == buffer.FREE:
+                    buffer.write(self.simulation[j], self.table)
+                    j += 1
+
+                action = buffer.read()
 
         print('Simulation terminated')
