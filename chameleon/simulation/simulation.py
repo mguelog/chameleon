75d438ee7bbc308b3ea56279cdc56440900bf058
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-06-11 01:58:28 +0200

    feat(simulation): wireshark support

--- chameleon/simulation/simulation.py
+++ chameleon/simulation/simulation.py
@@ -1,16 +1,16 @@
 from mininet.net import Mininet
 from mininet.cli import CLI
-from minicps.mcps import MiniCPS
 import sys
 
 
-class Simulation(MiniCPS):
-    def __init__(self, name, devices, topo, manager, cli):
+class Simulation:
+    def __init__(self, name, devices, topo, manager, cli, wireshark):
         self.name = name
         self.devices = devices
         self.net = Mininet(topo=topo)
         self.manager = manager
         self.cli = cli
+        self.wireshark = wireshark
 
     def start(self):
         self.net.start()
@@ -19,10 +19,14 @@ class Simulation(MiniCPS):
             device = self.net.get(device_name)
             device.cmd(sys.executable + ' -u {}.py &> logs/{}.log &'.format(device_name, device_name))
 
+        self.net.pingAll()
+
+        if self.wireshark:
+            CLI.do_sh(self.net, line=['wireshark &> logs/wireshark.log &'])
+        elif self.cli or self.manager is None:
+            CLI(self.net)
+
         if self.manager is not None:
             self.manager.simulate()
-
-        if self.cli or self.manager is None:
-            CLI(self.net)
 
         self.net.stop()
