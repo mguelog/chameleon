c94de975d55df71a7acb89d2860a2344ad637d00
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-07-06 13:13:31 +0200

    refactor(simulation): action buffer

--- chameleon/simulation/buffer.py
+++ chameleon/simulation/buffer.py
@@ -1,7 +1,8 @@
-from chameleon.simulation.logger import Logger
 import time
 
-logger = Logger()
+
+def short_delay():
+    time.sleep(0.05)
 
 
 class Buffer:
@@ -11,32 +12,34 @@ class Buffer:
     EXIT = 'exit'
     SLEEP = 0.2
 
-    def write(self, action, table):
-        logger.log_state(action, table)
-
+    def write(self, action):
         with open(self.BUFFER, 'w') as file:
             file.write(action)
 
     def free(self):
-        self.delay()
+        short_delay()
 
         with open(self.BUFFER, 'w') as file:
             file.write(self.FREE)
 
     def wait(self):
-        self.delay()
+        short_delay()
 
-        with open(self.BUFFER, 'w') as file:
-            file.write(self.WAIT)
+        if self.read() != self.FREE:
+            with open(self.BUFFER, 'w') as file:
+                file.write(self.WAIT)
 
     def create(self):
         self.free()
 
     def read(self):
-        self.delay()
+        short_delay()
 
         with open(self.BUFFER, 'r') as file:
             return file.readline()
+
+    def is_free(self):
+        return self.read() == self.FREE
 
     def delay(self):
         time.sleep(self.SLEEP)
