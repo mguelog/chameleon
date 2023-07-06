4540414fe2b83fdee36358a97c7add13906a1164
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-07-06 13:52:54 +0200

    feat(simulation): cycle log

--- chameleon/simulation/logger.py
+++ chameleon/simulation/logger.py
@@ -1,26 +1,46 @@
-from chameleon import STATE_LOG
+from chameleon import STATE_LOG, CYCLE_LOG
 import sqlite3
 import fnmatch
 import os
 
 
-def get_first(tuple):
-    return tuple[0]
+def get_first(tpl):
+    return tpl[0]
+
+
+select_all = 'SELECT value FROM {}'
 
 
 class Logger:
-    select = 'SELECT value FROM {}'
 
     def __init__(self):
         for file in os.listdir('.'):
             if fnmatch.fnmatch(file, '*.sqlite'):
-                connection = sqlite3.connect(file)
-                self.cursor = connection.cursor()
+                self.connection = sqlite3.connect(file)
+                self.cursor = self.connection.cursor()
                 break
 
-    def log_state(self, action, table):
-        self.cursor.execute(self.select.format(table))
-        state = list(map(get_first, self.cursor.fetchall()))
+    def get_values(self, select, table):
+        if table is not None:
+            self.cursor.execute(select.format(table))
+            return list(map(get_first, self.cursor.fetchall()))
+        else:
+            return None
 
-        with open(STATE_LOG, 'a') as file:
-            file.write(str(state) + '\n' + action + '\t')
+    def log_state(self, action, table):
+        state = self.get_values(select_all, table)
+
+        if state is not None:
+            with open(STATE_LOG, 'a') as file:
+                file.write(str(state) + '\n' + action + '\t')
+
+    def log_cycle(self, action, table):
+        if action is not None:
+            with open(CYCLE_LOG, 'a') as file:
+                file.write(action + '\n')
+        else:
+            state = self.get_values(select_all, table)
+
+            if state is not None:
+                with open(CYCLE_LOG, 'a') as file:
+                    file.write(str(state) + '\n')
