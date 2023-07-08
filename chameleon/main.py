d4e3a2ced569933b2bd09039a68f497c2dabf712
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-07-08 15:22:48 +0200

    refactor(CLI): run as root

--- chameleon/main.py
+++ chameleon/main.py
@@ -7,16 +7,18 @@ import subprocess
 def main():
     if args.command == 'init':
         if args.overwrite:
-            subprocess.run(['rm'] + glob.glob('*.sqlite'))
+            subprocess.run(['sudo', 'rm'] + glob.glob('*.sqlite'))
+
         try:
             subprocess.run(['python3.10', 'init.py'])
         except FileNotFoundError:
             print('Not init.py found')
     elif args.command == 'simulate':
         if args.log:
-            subprocess.run(['rm', STATE_LOG, CYCLE_LOG])
+            subprocess.run(['sudo', 'rm', STATE_LOG, CYCLE_LOG])
+
         try:
-            subprocess.run(['python3.10', 'simulate.py'])
+            subprocess.run(['sudo', 'python3.10', 'simulate.py'])
         except FileNotFoundError:
             print('Not simulate.py found')
 
