a6a3105dbd7e9e40f5cd6566b160ba90327e45e2
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-07-21 00:35:55 +0200

    feat(microgrid): set solar power

--- microgrid/solar.py
+++ microgrid/solar.py
@@ -4,8 +4,38 @@ from utils import *
 
 buffer = Buffer()
 
+SOLAR_ARRAY_POWER = ('SOLAR_ARRAY_POWER', 1)
+
+TIME = ('TIME', 1)
+
+MAX_POWER = 50
+RISING_SUN = 25200
+FULL_SUN = 36000
+FALLING_SUN = 57600
+NO_SUN = 68400
+
+
+def set_solar_power(self):
+    time = self.get(TIME)
+    power = 0
+
+    if RISING_SUN <= time < FULL_SUN:
+        power = round((time - RISING_SUN) / (FULL_SUN - RISING_SUN) * MAX_POWER, 2)
+    elif FULL_SUN <= time <= FALLING_SUN:
+        power = MAX_POWER
+    elif FALLING_SUN <= time <= NO_SUN:
+        power = round((1 - (time - FALLING_SUN) / (NO_SUN - FALLING_SUN)) * MAX_POWER, 2)
+
+    self.set(SOLAR_ARRAY_POWER, power)
+    print('DEBUG: {} set SOLAR_ARRAY_POWER: {}'.format(SOLAR_ARRAY, power))
+
+    buffer.free()
+
+
 if __name__ == '__main__':
     solar_array = Device(name=SOLAR_ARRAY,
                          state=STATE,
                          protocol=SOLAR_ARRAY_PROTOCOL,
-                         actions={})
+                         actions={
+                             SET_SOLAR_POWER: set_solar_power
+                         })
--- microgrid/utils.py
+++ microgrid/utils.py
@@ -96,7 +96,7 @@ DIESEL_GENERATOR_PROTOCOL = {
 SOLAR_ARRAY_MAC = '00:00:00:00:00:06'
 SOLAR_ARRAY_ADDR = '10.0.0.6'
 SOLAR_ARRAY_TAGS = (
-)
+    ('SOLAR_ARRAY_POWER', 1, 'REAL'),)
 SOLAR_ARRAY_SERVER = {
     'address': SOLAR_ARRAY_ADDR,
     'tags': SOLAR_ARRAY_TAGS
@@ -137,22 +137,26 @@ INIT_SCHEMA = """
     INSERT INTO microgrid_table VALUES (1, 'ENERGY_STORAGE_VOLTAGE', 6500);
     INSERT INTO microgrid_table VALUES (1, 'ENERGY_STORAGE_CURRENT', 0);
     INSERT INTO microgrid_table VALUES (1, 'ENERGY_STORAGE_ENERGY', 250);
+    INSERT INTO microgrid_table VALUES (1, 'SOLAR_ARRAY_POWER', 0);
     INSERT INTO microgrid_table VALUES (1, 'LOAD_DEMAND_POWER', 500);
 """.format(SECONDS_PER_TICK)
 
-# events
+# variables
+UTILITY_GRID_MAX_POWER = 500
+SECONDS_A_DAY = 86400
+ENERGY_STORAGE_MAX_ENERGY = 250
+TICK_TIME = SECONDS_PER_TICK / 3600
+
+# cycle actions
 CLOCK_TICK = 'clock_tick'
 
 SET_GRID_VOLTAGE = 'set_grid_voltage'
+SET_SOLAR_POWER = 'set_solar_power'
 
 SET_LOAD = 'set_load'
 PEAK_SHAVING = 'peak_shaving'
 CONSUME_BATTERY = 'consume_battery'
 
+# external actions
 TOGGLE_ISLAND = 'toggle_island'
 TOGGLE_PEAK_SHAVING = 'toggle_peak_shaving'
-
-# variables
-UTILITY_GRID_MAX_POWER = 500
-SECONDS_A_DAY = 86400
-TICK_TIME = SECONDS_PER_TICK / 3600
