95cbb9534e48cf3ea91530faf820b79da59ef73e
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-07-21 02:11:51 +0200

    feat(microgrid): set load by time demand

--- microgrid/load.py
+++ microgrid/load.py
@@ -7,19 +7,43 @@ buffer = Buffer()
 
 LOAD_DEMAND_POWER = ('LOAD_DEMAND_POWER', 1)
 
-MAX_POWER_INCREASE = 50
+TIME = ('TIME', 1)
+
+MIN_POWER_DEMAND = 250
+BASE_POWER_DEMAND = 500
+POWER_VARIATION = 25
+
+APEX_1 = 18000
+APEX_2 = 36000
+APEX_3 = 50400
+APEX_4 = 64800
+APEX_5 = 75600
+APEX_6 = 86400
+
+
+def time_demand(time):
+    index = 0
+
+    if time < APEX_1:
+        index = 0.5 - 0.5 * time / APEX_1
+    elif time < APEX_2:
+        index = 0.8 * (time - APEX_1) / (APEX_2 - APEX_1)
+    elif time < APEX_3:
+        index = 0.8 + 0.2 * (time - APEX_2) / (APEX_3 - APEX_2)
+    elif time < APEX_4:
+        index = 1 - 0.2 * (time - APEX_3) / (APEX_4 - APEX_3)
+    elif time < APEX_5:
+        index = 0.8 + 0.2 * (time - APEX_4) / (APEX_5 - APEX_4)
+    elif time < APEX_6:
+        index = 1 - 0.5 * (time - APEX_5) / (APEX_6 - APEX_5)
+
+    return index
 
 
 def set_load(self):
-    power = self.get(LOAD_DEMAND_POWER)
-    power_increase = random.uniform(-MAX_POWER_INCREASE, MAX_POWER_INCREASE)
-
-    if power + power_increase < 350:
-        power = round(power - power_increase, 2)
-    elif power + power_increase > 650:
-        power = round(power - power_increase, 2)
-    else:
-        power = round(power + power_increase, 2)
+    time = self.get(TIME)
+    power = MIN_POWER_DEMAND + time_demand(time) * BASE_POWER_DEMAND
+    power = round(power + random.randint(-POWER_VARIATION, POWER_VARIATION), 2)
 
     self.set(LOAD_DEMAND_POWER, power)
     print('DEBUG: {} set LOAD_DEMAND_POWER: {}'.format(LOAD_DEMAND, power))
