d17bf62fed748e6302b1d1c8a421ab64752c09d4
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-07-27 03:40:03 +0200

    feat(microgrid): manager

--- microgrid/manager.py
+++ microgrid/manager.py
@@ -4,10 +4,27 @@ from utils import *
 
 buffer = Buffer()
 
-cycle = []
+cycle_actions = [
+    CLOCK_TICK,
+    SET_GRID_VOLTAGE,
+    SET_SOLAR_POWER,
+    SET_LOAD,
+    PEAK_SHAVING,
+    CONSUME_BATTERY,
+    GENERATOR_SUPPLY,
+    RELOAD_BATTERY,
+    NIGHT_RELOAD
+]
+
+external_actions = [
+    TOGGLE_ISLAND,
+    TOGGLE_PEAK_SHAVING,
+    TOGGLE_CLOUDY,
+    REFUEL_GENERATOR
+]
 
 manager = Manager(
-    cycle_actions=cycle,
-    external_actions={},
-    cycles=1,
+    cycle_actions=cycle_actions,
+    external_actions=external_actions,
+    cycles=2880,
     table=NAME)
