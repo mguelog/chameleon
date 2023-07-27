4d08414a17f7eb608e790ad1fc07cc79ac272728
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-07-27 20:00:27 +0200

    feat(simulation): start collection

---
+++ chameleon/simulation/collection.py
@@ -0,0 +1,24 @@
+from mininet.net import Mininet
+import sys
+
+
+class Collection:
+    def __init__(self, name, devices, topo, manager):
+        self.name = name
+        self.devices = devices
+        self.net = Mininet(topo=topo)
+        self.manager = manager
+
+    def start(self):
+        self.net.start()
+
+        for device_name in self.devices:
+            device = self.net.get(device_name)
+            device.cmd(sys.executable + ' -u {}.py &> logs/{}.log &'.format(device_name, device_name))
+
+        self.net.pingAll()
+
+        if self.manager is not None:
+            self.manager.collect()
+
+        self.net.stop()
