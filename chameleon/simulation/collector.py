63af659986c5f4a4e52d758b2f6e8d59d8c6b12d
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-07-27 20:01:11 +0200

    feat(simulation): data collector

---
+++ chameleon/simulation/collector.py
@@ -0,0 +1,25 @@
+from chameleon.simulation.state import State
+from chameleon.simulation.dataset import Dataset
+
+
+class Collector:
+
+    def __init__(self, manager, table):
+        self.state = State(table)
+        self.dataset = Dataset(table)
+        self.manager = manager
+
+    def set_state(self, updates):
+        self.state.set_state(updates)
+
+    def run_cycles(self, cycles, collect_cycle, select):
+        self.manager.cycle_loop(cycles, collect_cycle, select)
+
+    def run_action(self, action, collect_action):
+        self.manager.run_action(action, collect_action)
+
+    def check_constrain(self, constrain, expected_value):
+        self.state.check_constrain(constrain, expected_value)
+
+    def new_row(self):
+        self.dataset.new_line()
