e82a58d28e6aa5badd53b5cae42f603b2e5f69ad
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-08-10 01:51:17 +0200

    refactor(microgrid): collect.py renaming

--- microgrid/control.py
+++ microgrid/control.py
@@ -1,13 +1,13 @@
-from chameleon.simulation.collection import Collection
+from chameleon.simulation.control import Control
 from utils import MICROGRID_CONTROLLER, UTILITY_GRID, ENERGY_STORAGE, LOAD_DEMAND, DIESEL_GENERATOR, SOLAR_ARRAY
 from manager import manager
 from topo import topo
 
-collection = Collection(
+control = Control(
     name='microgrid',
     devices=[MICROGRID_CONTROLLER, UTILITY_GRID, ENERGY_STORAGE, LOAD_DEMAND, DIESEL_GENERATOR, SOLAR_ARRAY],
     topo=topo,
     manager=manager)
 
 if __name__ == '__main__':
-    collection.start()
+    control.start()
