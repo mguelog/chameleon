fd0719be2a9b70e9b52ede6691c60f9da445d1ce
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-08-10 05:03:26 +0200

    fix(simulation): control start

--- chameleon/simulation/control.py
+++ chameleon/simulation/control.py
@@ -7,7 +7,7 @@ class Control:
     def __init__(self, name, devices, devices_directory, topo, manager):
         self.name = name
         self.devices = devices
-        self.devices_directory = devices_directory,
+        self.devices_directory = devices_directory
         self.net = Mininet(topo=topo)
         self.manager = manager
 
@@ -19,9 +19,9 @@ class Control:
             device.cmd(sys.executable + ' -u {}/{}.py &> logs/{}.log &'.format(self.devices_directory,
                                                                                device_name, device_name))
 
-            self.net.pingAll()
+        self.net.pingAll()
 
-            if self.manager is not None:
-                self.manager.control()
+        if self.manager is not None:
+            self.manager.control()
 
-            self.net.stop()
+        self.net.stop()
