9d9ef32d02dbd44d132b80d9618315a42318507d
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-08-30 03:44:22 +0200

    refactor(simulation): device main loop

--- chameleon/simulation/device.py
+++ chameleon/simulation/device.py
@@ -1,28 +1,27 @@
 from minicps.devices import PLC
 from chameleon.simulation.buffer import Buffer
 
-buffer = Buffer()
-
 
 class Device(PLC):
 
     def __init__(self, name, protocol, state, actions):
         self.actions = actions
+        self.buffer = Buffer()
         PLC.__init__(self, name, protocol, state)
 
     def pre_loop(self, sleep=0):
         print('DEBUG: initializing device')
 
     def main_loop(self, sleep=0):
-        print('DEBUG: executing transitions')
+        print('DEBUG: executing actions')
 
-        action = buffer.read()
+        action = self.buffer.read()
 
-        while not buffer.is_exited():
+        while not self.buffer.is_exited():
 
-            if action not in [buffer.FREE, buffer.WAIT] and action in self.actions:
+            if action in self.actions:
                 self.actions[action](self)
 
-            action = buffer.read()
+            action = self.buffer.read()
 
-        print('DEBUG: terminated executing transitions')
+        print('DEBUG: terminated executing actions')
